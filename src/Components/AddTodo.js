66d30f142468882a75c0f87121b08373330eff03
Tommy Yates <7tjyates7@gmail.com>
2020-02-12 12:53:39 -0500

    Corrected Date and Validated Due Date

--- src/Components/AddTodo.js
+++ src/Components/AddTodo.js
@@ -5,11 +5,35 @@ export class AddTodo extends Component {
         title: '',
         dueDate: '',
     }
+    todaysDate = () => {
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+
+        today = mm + '/' + dd + '/' + yyyy; 
+        return today
+    }
+    tomorrowsDate = () => {
+        var today = new Date();
+        var dd = String(today.getDate()+1).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+
+        today = mm + '/' + dd + '/' + yyyy; 
+        return today
+    }
 
     onSubmit = (e) => {
         e.preventDefault();
-        this.props.addTodo(this.state.title, this.state.dueDate);
-        this.setState({title: '', dueDate: ''})
+        const regex = /^(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d$/g;
+        if (this.state.dueDate.match(regex)=== null){
+            alert('Please Input a Valid Due Date: mm/dd/yyyy')
+            this.setState({dueDate: ''})
+        } else {
+            this.props.addTodo(this.state.title, this.state.dueDate);
+            this.setState({title: '', dueDate: ''})
+        }
     }
 
     onChange = (e) => this.setState({ [e.target.name]: e.target.value});
